cdaf74fcaf561dc750459bb0cd43ceb5e33db4dd
pt-kt <tany162@mail.ru>
2025-05-02 16:20:19 +0300

    дороботано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -30,90 +30,51 @@ public class Main {
             System.out.println(year + " не является високосным");
         }
         System.out.println("Задача 4");
-        int deliveryDistance = 20;
-        int timeOneZoneDelivery = 1; {
+        int deliveryDistance = 101;
+        int timeOneZoneDelivery = 1;
+
         if (deliveryDistance > 100) {
-            System.out.println("Доставки нет"); {
-                return;
-            }
-        } else if (deliveryDistance > 0 && deliveryDistance < 20) {
-            timeOneZoneDelivery = 1;
-        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            timeOneZoneDelivery += 1;
-        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            System.out.println("Доставки нет");
+
+        } else if (deliveryDistance >60) {
             timeOneZoneDelivery += 2;
+        } else if (deliveryDistance >20) {
+            timeOneZoneDelivery += 1;}
+        {
+            System.out.println("Потребуется дней доставки: " + timeOneZoneDelivery);
         }
-        System.out.println("Потребуется дней: " + timeOneZoneDelivery);
-    }
 
         System.out.println("Задача 5");
-        int monthNumber = 10;
+        int monthNumber = 1;
 
-        switch (monthNumber) {
-            case 1:
-                System.out.println("Январь");
-                break;
-            case 2:
-                System.out.println("Февраль");
-                break;
-            case 3:
-                System.out.println("Март");
-                break;
-            case 4:
-                System.out.println("Апрель");
-                break;
-            case 5:
-                System.out.println("Май");
-                break;
-            case 6:
-                System.out.println("Июнь");
-                break;
-            case 7:
-                System.out.println("Июль");
-                break;
-            case 8:
-                System.out.println("Август");
-                break;
-            case 9:
-                System.out.println("Сентябрь");
-                break;
-            case 10:
-                System.out.println("Октябрь");
-                break;
-            case 11:
-                System.out.println("Ноябрь");
-                break;
-            case 12:
-                System.out.println("Декабрь");
-                break;
-            default:
-                System.out.println("Такого дня не существует");
-        }
-        switch (monthNumber) {
-            case 1:
-            case 2:
-            case 12:
-                System.out.println("Сезон зима");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Сезон весна");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Сезон лето");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Сезон осень");
-                break;
-        }
-
+        switch(monthNumber) {
+        case 1:
+        case 2:
+        case 12:
+        System.out.println("Сезон зима");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            System.out.println("Сезон весна");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            System.out.println("Сезон лето");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            System.out.println("Сезон осень");
+            break;
+        default:
+            System.out.println("Такого дня не существует");
         }
     }
+}
+
+
 
 
 
